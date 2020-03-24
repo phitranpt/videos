1adc6eb6c47301d1889a68a8476652be45db7d4c
phitranpt <phi.tran.pt@gmail.com>
2020-03-24 16:19:17 -0500

    added VideoItems

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,5 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAM6nE7koN_gyo5Vd24XU2WQqk5tvuQQh8';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    maxResults: 5,
-    key: KEY,
-  }
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyAM6nE7koN_gyo5Vd24XU2WQqk5tvuQQh8';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
 
+  state = {
+    videos: []
+  };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+      }
+    });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
