2f7077670c60cc3d6cc4b7d80a05164b115eefdb
phitranpt <phi.tran.pt@gmail.com>
2020-03-28 18:08:30 -0500

    fixed onTermSubmit and selectedVideo

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,10 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount () {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
@@ -22,7 +26,10 @@ class App extends React.Component {
         key: KEY,
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items, 
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
@@ -35,11 +42,21 @@ class App extends React.Component {
         <SearchBar 
           onFormSubmit={this.onTermSubmit} 
         />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList 
-          onVideoSelect={this.onVideoSelect} 
-          videos={this.state.videos} 
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail 
+                video={this.state.selectedVideo}
+              />
+            </div>
+            <div className="five wide column">
+              <VideoList 
+                onVideoSelect={this.onVideoSelect} 
+                videos={this.state.videos} 
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
